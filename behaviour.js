e4e9e27a8e26bd1f9acc9d46d73985cbcc12003e
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2014-04-18 00:10:03 -0400

    Late night at Chicas Poderosas with Michael commiting the wtfwww

---
+++ behaviour.js
@@ -0,0 +1,32 @@
+
+function say(text) {
+    $("#bubble p").html(text);
+    $("#bubble").show();
+
+
+    var speak=function(count) {
+        if (count>0) {
+        $("#lips").toggleClass("open");
+        setTimeout(function() {speak(count-1)},200);
+    }
+    else {
+        $("#lips").removeClass("open");
+        setTimeout(function(){$("#bubble").hide();},2000);
+            }
+    };
+
+    speak(text.split(" ").length*2);
+    window.location.href="#"+encodeURI(text);
+};
+
+$("form").on("submit",function(){
+    say($("#text").val() || "Say What?");
+    return false;
+});
+
+
+$(document).ready(function() {
+    if (window.location.hash) {
+        say(decodeURI(window.location.hash.substring(1)));
+    }
+});
